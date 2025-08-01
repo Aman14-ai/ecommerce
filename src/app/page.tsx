1619a55fde47bd94f1234fdb5c48f63b4a221dad
Aman14-ai <amancode2005@gmail.com>
2025-08-02 02:29:04 +0530

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import ProductCard from "@/components/ProductCard";
 import { prisma } from "@/lib/db/prisma";
-import Image from "next/image";
 
 export default async function Home() {
   const products = await prisma.product.findMany({orderBy: {createdAt: "desc"}});
